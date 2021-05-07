c74d5088b8305ebddc89f332469da150e6040bdc
kai.zheng <kai.zheng@joyme.tech>
2021-05-07 16:35:06 +0800

    kafka练习之批量发送消息

---
+++ lab-001-kafka-batch-produce/src/main/java/com/iteasy/kafka/Lab001KafkaBatchProduceApplication.java
@@ -0,0 +1,13 @@
+package com.iteasy.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab001KafkaBatchProduceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab001KafkaBatchProduceApplication.class, args);
+    }
+
+}
